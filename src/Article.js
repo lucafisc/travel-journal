39e09b6cace958adda8d23acc3da34b345aaaff3
lucafisc <lucafischer@fastmail.com>
2022-08-24 10:35:53 +0200

    Render data

--- src/Article.js
+++ src/Article.js
@@ -1,3 +1,14 @@
-export default function Article() {
-  return <section className="article"></section>;
+export default function Article(props) {
+  console.log(props);
+  return (
+    <section className="article">
+      <img src={props.imageUrl} />
+      <h2>{props.location}</h2>
+      <a src={props.googleMapsUrl}>View on Google Maps</a>
+      <h1>{props.title}</h1>
+      <p>{props.startDate}</p>
+      <p>{props.endDate}</p>
+      <p>{props.description}</p>
+    </section>
+  );
 }
