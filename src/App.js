25b14581bdb55121919679023d79124ce1eef121
lucafisc <lucafischer@fastmail.com>
2022-08-24 10:30:54 +0200

    Add Nav and Article

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./Nav";
+import Article from "./Article";
+import data from "./data";
 
 function App() {
+  const articles = data.map((item) => {
+    return <Article key={item.id} {...item} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <section>{articles}</section>
+    </>
   );
 }
 
