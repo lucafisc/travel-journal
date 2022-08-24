e602a87d5b5d1284cc688b604fdbbc73e235a813
lucafisc <lucafischer@fastmail.com>
2022-08-24 12:00:48 +0200

    Update Article component

--- src/Article.js
+++ src/Article.js
@@ -1,14 +1,19 @@
 export default function Article(props) {
-  console.log(props);
   return (
     <section className="article">
-      <img src={props.imageUrl} alt={props.alt} />
-      <h2>{props.location}</h2>
-      <a src={props.googleMapsUrl}>View on Google Maps</a>
-      <h1>{props.title}</h1>
-      <p>{props.startDate}</p>
-      <p>{props.endDate}</p>
-      <p>{props.description}</p>
+      <img className="article--cover" src={props.imageUrl} alt={props.alt} />
+      <div className="article--text">
+        <div className="article--location">
+          <h2>{props.location}</h2>
+          <a src={props.googleMapsUrl}>View on Google Maps</a>
+        </div>
+        <h1 className="article--title">{props.title}</h1>
+        <div className="article--dates">
+          <p>{props.startDate}</p>
+          <p>{props.endDate}</p>
+        </div>
+        <p className="article-description">{props.description}</p>
+      </div>
     </section>
   );
 }
