7a145c79c83391b2f70375c27839ffa2781acce0
lucafisc <lucafischer@fastmail.com>
2022-08-24 10:42:17 +0200

    Add unique key to article

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import Nav from "./Nav";
 import Article from "./Article";
 import data from "./data";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const articles = data.map((item) => {
-    return <Article key={item.id} {...item} />;
+    return <Article key={uuidv4()} {...item} />;
   });
 
   return (
