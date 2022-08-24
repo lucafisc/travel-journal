584fef4f38cb59e0cf14a0d7fa27028868007f75
lucafisc <lucafischer@fastmail.com>
2022-08-24 10:41:56 +0200

    Add alt to image

--- src/Article.js
+++ src/Article.js
@@ -2,7 +2,7 @@ export default function Article(props) {
   console.log(props);
   return (
     <section className="article">
-      <img src={props.imageUrl} />
+      <img src={props.imageUrl} alt={props.alt} />
       <h2>{props.location}</h2>
       <a src={props.googleMapsUrl}>View on Google Maps</a>
       <h1>{props.title}</h1>
