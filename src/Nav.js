1afa02a676f5d3771471f1acf0334d097674965f
lucafisc <lucafischer@fastmail.com>
2022-08-24 10:20:42 +0200

    Add Nav component

---
+++ src/Nav.js
@@ -0,0 +1,9 @@
+import logo from "./images/logo.png";
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <img src={logo} />
+      <h1>my travel journal</h1>
+    </nav>
+  );
+}
