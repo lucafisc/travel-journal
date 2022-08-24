472dc4baaa85311098027c3721b6cd7e5ccff418
lucafisc <lucafischer@fastmail.com>
2022-08-24 10:41:48 +0200

    Add alt data

--- src/data.js
+++ src/data.js
@@ -8,6 +8,7 @@ export default [
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    alt: "snow covered mountain under cloudy sky during daytime photo",
   },
   {
     title: "Sydney Opera House",
@@ -18,6 +19,7 @@ export default [
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
     imageUrl: "https://source.unsplash.com/TkEBaFrYJ_g",
+    alt: "Sydney opera house during daytime",
   },
   {
     title: "Geirangerfjord",
@@ -28,5 +30,6 @@ export default [
     description:
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
     imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+    alt: "Geirangerfjord region from above",
   },
 ];
